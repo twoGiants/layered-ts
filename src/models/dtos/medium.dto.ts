e47fd8556668f6d03cb206d1cea3b08a76f98dba
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-01-04 16:28:48 +0100

    added dtos

---
+++ src/models/dtos/medium.dto.ts
@@ -0,0 +1,8 @@
+export interface MediumDto {
+  firsname: string;
+  lastname: string;
+  age: number;
+  nationality: string;
+  dateOfBirth: Date;
+  placeOfBirth: string;
+}
---
+++ src/models/dtos/simple.dto.ts
@@ -0,0 +1,4 @@
+export interface SimpleDto {
+  firsname: string;
+  lastname: string;
+}

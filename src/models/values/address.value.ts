30d34d4b732d8a2cdd8babe91b8ca5b28d4bf6e6
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-01-05 13:03:26 +0100

    added address value object

---
+++ src/models/values/address.value.ts
@@ -0,0 +1,33 @@
+import { Country } from "../enums/country.enum";
+
+export class Address {
+  #streetname: string;
+  #streetnumber: number;
+  #city: string;
+  #country: Country;
+  #zip: string;
+
+  constructor(
+    streetname: string,
+    streetnumber: number,
+    city: string,
+    country: Country,
+    zip: string,
+  ) {
+    this.#streetname = streetname;
+    this.#streetnumber = streetnumber;
+    this.#city = city;
+    this.#country = country;
+    this.#zip = zip;
+  }
+
+  get copy() {
+    return new Address(
+      this.#streetname,
+      this.#streetnumber,
+      this.#city,
+      this.#country,
+      this.#zip,
+    );
+  }
+}

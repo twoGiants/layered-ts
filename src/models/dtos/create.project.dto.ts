879dcff93a6a9b136e60bd1bb74d36fa12b8997e
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-01-05 13:07:27 +0100

    added create project dto

---
+++ src/models/dtos/create.project.dto.ts
@@ -0,0 +1,9 @@
+export interface CreateProjectDto {
+  title: string;
+  streetname: string;
+  streetnumber: number;
+  city: string;
+  country: string;
+  zip: string;
+  responsibleId: string;
+}

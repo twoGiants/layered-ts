d7e06e27d66d03f12bcffefcdcd05783be07a28f
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-01-05 13:07:17 +0100

    removed dependencies

--- src/models/dtos/create.employee.dto.ts
+++ src/models/dtos/create.employee.dto.ts
@@ -1,9 +1,6 @@
-import { Department } from "../enums/department.code.enum";
-import { JobRole } from "../enums/job.role.enum";
-
 export interface CreateEmployeeDto {
   firstname: string;
   lastname: string;
-  department: Department;
-  jobRole: JobRole;
+  department: string;
+  jobRole: string;
 }

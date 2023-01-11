b7bc9414023548d871f9a2379bb5395edc776216
Stanislav Jakuschevskij <stas@two-giants.com>
2023-01-11 17:59:15 +0100

    implemented service

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,52 @@
+import { ApplicationException } from "@app/exceptions/application.exception";
+import { EmployeeRepository } from "@app/repositories/emploee.repository.interface";
+import { ProjectRepository } from "@app/repositories/project.repository.interface";
+import { CreateProjectDto } from "@core/dtos/create.project.dto";
+import { ProjectEntity } from "@core/entities/project.entity";
+import { Country } from "@core/enums/country.enum";
+import { Address } from "@core/values/address.value";
+
+export interface ProjectService {
+  createProject(dto: CreateProjectDto): Promise<ProjectEntity>;
+}
+
+export class ProjectServiceImpl implements ProjectService {
+  #employeeRepository: EmployeeRepository;
+  #projectRepository: ProjectRepository;
+
+  constructor(
+    employeeRepository: EmployeeRepository,
+    projectRepository: ProjectRepository,
+  ) {
+    this.#employeeRepository = employeeRepository;
+    this.#projectRepository = projectRepository;
+  }
+
+  // Rich Service Layer: Orchestration + Domain Logic
+  // - validate incomming data
+  // - get everything you need to continue
+  // - construct domain instances
+  // - perform domain logic
+  // - update state
+  async createProject(dto: CreateProjectDto): Promise<ProjectEntity> {
+    const projectResponsible = await this.#employeeRepository.loadById(dto.responsibleId);
+
+    if (!projectResponsible) {
+      throw new ApplicationException(
+        `Document not found. Provided id: ${dto.responsibleId}`,
+      );
+    }
+
+    const projectAddress = new Address(
+      dto.streetname,
+      dto.streetnumber,
+      dto.city,
+      <Country>dto.country,
+      dto.zip,
+    );
+
+    const newProject = new ProjectEntity(dto.title, projectAddress, projectResponsible);
+
+    return this.#projectRepository.createProject(newProject);
+  }
+}

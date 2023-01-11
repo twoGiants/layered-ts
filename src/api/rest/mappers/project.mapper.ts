1d8dc82f3b321113680fd985e49d23cbe177a068
Stanislav Jakuschevskij <stas@two-giants.com>
2023-01-11 17:59:05 +0100

    implemented output mapper

---
+++ src/api/rest/mappers/project.mapper.ts
@@ -0,0 +1,13 @@
+import { ProjectEntity } from "@core/entities/project.entity";
+import { ProjectOutputDto } from "@api/rest/dtos/project.output.dto";
+
+export class ProjectMapper {
+  static toOutputDto(project: ProjectEntity): ProjectOutputDto {
+    return {
+      id: project.id,
+      title: project.title,
+      location: project.location,
+      responsible: project.responsible,
+    };
+  }
+}

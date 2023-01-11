2dbdf628cfba36e7736a323f3becb8fd1c56f402
Stanislav Jakuschevskij <stas@two-giants.com>
2023-01-11 17:59:25 +0100

    implemented project controller

---
+++ src/api/rest/controllers/project.controller.ts
@@ -0,0 +1,19 @@
+import { ProjectService } from "@app/services/project.service";
+import { CreateProjectDto } from "@core/dtos/create.project.dto";
+import { ProjectMapper } from "@api/rest/mappers/project.mapper";
+import { ProjectOutputDto } from "@api/rest/dtos/project.output.dto";
+
+export class ProjectController {
+  #projectService: ProjectService;
+
+  constructor(projectService: ProjectService) {
+    this.#projectService = projectService;
+  }
+
+  // POST /project
+  async createProject(createProjectDto: CreateProjectDto): Promise<ProjectOutputDto> {
+    const project = await this.#projectService.createProject(createProjectDto);
+
+    return Promise.resolve(ProjectMapper.toOutputDto(project));
+  }
+}

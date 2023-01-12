e0ae0326ccd4b58b351e643c298ec1e4344d3bc3
Stanislav Jakuschevskij <stas@two-giants.com>
2023-01-12 17:11:11 +0100

    updated repository interface
    # added return value

--- src/app/repositories/emploee.repository.interface.ts
+++ src/app/repositories/emploee.repository.interface.ts
@@ -1,6 +1,6 @@
 import { EmployeeEntity } from "@core/entities/employee.entity";
 
 export interface EmployeeRepository {
-  createEmployee(validEmployee: EmployeeEntity): Promise<void>;
+  createEmployee(validEmployee: EmployeeEntity): Promise<EmployeeEntity>;
   loadById(id: string): Promise<EmployeeEntity>;
 }

24c768a333025d608c4d9a30ec0e51cdfd49fba5
Stanislav Jakuschevskij <stas@two-giants.com>
2023-01-17 09:49:40 +0100

    implementede project controller

--- src/api/rest/controllers/project.controller.ts
+++ src/api/rest/controllers/project.controller.ts
@@ -1,19 +1,50 @@
+import { Request, Response, Router } from "express";
 import { CreateProjectDto } from "@core/dtos/create.project.dto";
 import { ProjectMapper } from "@api/rest/mappers/project.mapper";
-import { ProjectOutputDto } from "@api/rest/dtos/project.output.dto";
 import { ProjectService } from "@app/services/project/project.service.interface";
+import { ProjectRepository } from "@app/repositories/project.repository.interface";
 
 export class ProjectController {
-  #projectService: ProjectService;
+  static PATH = "/consulting";
 
-  constructor(projectService: ProjectService) {
+  router: Router;
+
+  #projectService: ProjectService;
+  #projectRepository: ProjectRepository;
+
+  constructor(projectService: ProjectService, projectRepository: ProjectRepository) {
+    this.router = Router();
     this.#projectService = projectService;
+    this.#projectRepository = projectRepository;
+
+    this.#initRoutes();
   }
 
-  // POST /project
-  async createProject(createProjectDto: CreateProjectDto): Promise<ProjectOutputDto> {
+  #initRoutes() {
+    // todo add types
+    this.router.post(`${ProjectController.PATH}/projects`, this.createProject);
+    this.router.get(`${ProjectController.PATH}/projects`, this.getProjects);
+    this.router.get(`${ProjectController.PATH}/projects/:id`, this.getProject);
+  }
+
+  createProject = async (request: Request, response: Response) => {
+    const createProjectDto: CreateProjectDto = request.body;
     const project = await this.#projectService.createProject(createProjectDto);
 
-    return Promise.resolve(ProjectMapper.toOutputDto(project));
-  }
+    response.json(ProjectMapper.toOutputDto(project));
+  };
+
+  getProject = async (request: Request, response: Response) => {
+    const projectId = request.params.id;
+
+    const project = await this.#projectRepository.loadById(projectId);
+
+    response.json(ProjectMapper.toOutputDto(project));
+  };
+
+  getProjects = async (_: Request, response: Response) => {
+    const projects = await this.#projectRepository.loadAll();
+
+    response.json(ProjectMapper.toOutputDtos(projects));
+  };
 }
